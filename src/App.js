609c46cd44962afd6fc4c55fd0b2ccc1c1645d36
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-24 13:23:17 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Book from './components/Book';
 import Form from './components/Form';
+import Categories from './pages/Categories';
+import NavBar from './components/NavBar';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Book />
-      <Form />
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <Book />
+            <Form />
+          </div>
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
