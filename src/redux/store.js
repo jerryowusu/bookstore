70b7062456ddb850c223fb88711997eb2dc0c600
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-28 15:35:49 +0000

    delete unused store.js file

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import rootReducer from './configureStore';
-
-const middlewares = [logger];
-
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
