1f0d5af55a240fdecdcd8b864893cfdd9855654e
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-28 15:59:30 +0000

    update categories reducer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,24 +1,17 @@
 const CHECK_STATUS = 'CHECK_STATUS';
 
-const INITIAL_STATE = {
-  categories: [],
-};
-
-const categoriesReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case CHECK_STATUS:
-      return {
-        ...state,
-        categories: 'under construction',
-      };
-    default:
-      return state;
-  }
-};
-
 export const setCategories = (categories) => ({
   type: CHECK_STATUS,
   payload: categories,
 });
+
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'under construction';
+    default:
+      return state;
+  }
+};
 
 export default categoriesReducer;
