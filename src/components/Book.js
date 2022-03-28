cecb39066c80e7c8b6d474d68844d6fbc85799dd
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-28 17:20:35 +0000

    add bookcard component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = () => (
+const Book = ({ name, author, category }) => (
 
-  <>
-    <div className="book">Book</div>
-    <div className="author">Author</div>
-    <button type="button">Remove book</button>
-  </>
+  <div className="book-card">
+    <div className="main-part">
+      <h4 className="category">{category}</h4>
+      <h2 className="book-name">{name}</h2>
+      <p className="author">{author}</p>
+      <li className="main-btns">
+        <button type="button" className="comment-btn btn">Comments</button>
+        <button type="button" className="remove-btn btn">Remove</button>
+        <button type="button" className="edit-btn btn">Edit</button>
+      </li>
+    </div>
+    <div className="progress-status">
+      <div className="progress-chart" />
+      <div>
+        <h2>50%</h2>
+        <p className="reading-status">Completed</p>
+      </div>
+    </div>
+    <div className="progress-update">
+      <p>Current Chapter</p>
+      <p>Chapter 12</p>
+      <button type="button" className="update-btn btn">UPDATE PROGRESS</button>
+    </div>
+  </div>
 
 );
+
+Book.propTypes = {
+  name: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
 
 export default Book;
---
+++ src/components/Books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from './Book';
+
+const Books = () => (
+  <>
+    <Book name="Rich Dad Poor Dad" author="Robert Kiyosaki" category="Business" />
+    <Book name="Happiness is a Choice" author="Robert Jerry Owusu" category="Business" />
+  </>
+
+);
+
+export default Books;
