a9c1ea6349cb2f9954ea37398d630ca70643e105
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-28 11:46:11 +0000

    create root reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+export default combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer,
+});
