12a1bc9dec0a5f0df34e82022abe82091b6f5dd9
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-29 12:22:03 +0000

    creating the list of books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Book from './Book';
 
-const Books = () => (
-  <>
-    <Book name="Rich Dad Poor Dad" author="Robert Kiyosaki" category="Business" />
-    <Book name="Happiness is a Choice" author="Robert Jerry Owusu" category="Business" />
-  </>
+const Books = ({ books }) => {
+  const booklists = books.map((book) => (
+    <Book
+      key={book.id}
+      name={book.name}
+      author={book.author}
+      category={book.category}
+    />
+  ));
 
-);
+  return (
+    booklists
+  );
+};
+
+Books.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default Books;
