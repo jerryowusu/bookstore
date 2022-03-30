6dae4d0b2725386fc004cc9c476bf778651b4838
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-30 00:56:40 -0700

    update categories page to use dispatch and useselector

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button type="submit">
-      Check Status
+const Page = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories);
+
+  return (
+    <button
+      type="button"
+      className="categories-btn"
+      onClick={() => {
+        dispatch(checkStatus('Under Construction, See you soon'));
+      }}
+    >
+      {status}
     </button>
-  </div>
-);
+  );
+};
 
-export default Categories;
+export default Page;
