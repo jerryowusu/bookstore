5ca46e30f8f26b6fcda4c2a23af9c26c8c4cf2ad
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-30 00:08:12 -0700

    update and add proptypes to books component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import BookCard from './BookCard';
 
-const Books = () => (
-  <>
-    <BookCard name="Rich Dad Poor Dad" author="Robert Kiyosaki" category="Business" />
-    <BookCard name="Happiness is a Choice" author="Robert Jerry Owusu" category="Business" />
-  </>
+const Books = ({ books }) => {
+  const booklists = books.map((book) => {
+    const key = book.id;
 
-);
+    return (
+      <BookCard
+        key={key}
+        id={book.id}
+        name={book.name}
+        author={book.author}
+        category={book.category}
+      />
+    );
+  });
+  return (
+    booklists
+  );
+};
+
+Books.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+Books.defaultProps = {
+  books: [],
+};
 
 export default Books;
